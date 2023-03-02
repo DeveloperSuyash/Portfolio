702e70842372fb288a26f37e56b004673e492333
Sdad-Developer <suyashsrivastav945@gmail.com>
2023-03-02 11:11:57 +0530

    First commit Message

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        
+        "<span>Hi, I am Suyash Srivastav</span>  and I am a web developer. I have a passion for creating beautiful, intuitive websites and web applications that provide a seamless user experience. My expertise includes programming languages such as HTML, CSS, JavaScript, and PHP, as well as experience with popular web development frameworks like React.<br>As a web developer, I am dedicated to ensuring that my code is efficient, optimized, and meets high standards of quality. I have a keen eye for design and aesthetics, and I believe that a visually appealing website can greatly enhance the user experience. I am a strong problem solver, and I take pride in troubleshooting and debugging code to ensure that my projects are error-free.<br>I enjoy collaborating with designers, developers, and other stakeholders to bring projects to completion. I have excellent communication skills, and I am able to explain technical concepts in a clear and concise manner to non-technical stakeholders.<br>Overall, I am committed to staying up-to-date with the latest web development trends and technologies to ensure that I am delivering cutting-edge solutions to my clients. I am excited to continue learning and growing as a web developer and to create exceptional websites and web applications that meet the needs of my clients and users."
+      
+      ],
+      typeSpeed: 10,
+      contentType: 'html'
+    });
+  }, 100);
